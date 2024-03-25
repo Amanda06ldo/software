b487ea75d231710c852f925a1641303012a2c3d7
Amanda06ldo <162368771+Amanda06ldo@users.noreply.github.com>
2024-03-25 16:50:28 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+/******************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+print('Hello World');
+var numero1 = 5;
+var numero2 = 8;
+var resultado ;
+resultado = numero1==numero2
+print(numero1, "=", numero2,"?",resultado)
+resultado = numero1 != numero2;
+print(numero1,"!=", numero2,"?",resultado);
+resultado = numero1 > numero2;
+print(numero1,">", numero2,"?",resultado);
+resultado = numero1 < numero2;
+print(numero1,"<", numero2,"?",resultado);
+var a1 = true && true;
+print(a1)
+var a2 = true && false;
+print(a2)
+var a3 = false && true;
+print(a3)
+var a4 = false && 3 == 4;
+print(a4)
+var a5 = "gato" && "cão";
+print(a5)
+var a6 = false && "gato";
+print(a6)
+var a7 = "gato" && false;
+print(a7)
+
+
+var a1 = true || true;
+print(a1)
+var a2 = true || false;
+print(a2)
+var a3 = false || true;
+print(a3)
+var a4 = false || 3 == 4;
+print(a4)
+var a5 = "gato" || "cão";
+print(a5)
+var a6 = false || "gato";
+print(a6)
+var a7 = "gato" || false;
+print(a7)
+
+var palavra1 = "aman"
+var palavra2 = "da"
+ var resultado2 = palavra1 + palavra2
+ print(resultado2)
+ var idade = 17
+ var status = idade >= 18 ? "adulto":"menor de idade";
+ print(status)
+ 
